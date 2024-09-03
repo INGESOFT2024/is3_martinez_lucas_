472c2d17ebf45f4cd0d6fa83130e7e26c03675b3
lucasartal56 <lucdanart@gmail.com>
2024-09-03 11:47:17 -0600

    correccion de errores en layout

--- views/layout.php
+++ views/layout.php
@@ -16,7 +16,7 @@
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarToggler" aria-controls="navbarToggler" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
-            <a class="navbar-brand" href="/ejemplo/">
+            <a class="navbar-brand" href="/is3_martinez_lucas_">
                 <img src="<?= asset('./images/emdn.png') ?>" width="35px'" alt="cit" >
                 Aplicaciones
             </a>
@@ -24,17 +24,17 @@
                 
                 <ul class="navbar-nav me-auto mb-2 mb-lg-0" style="margin: 0;">
                     <li class="nav-item">
-                        <a class="nav-link" aria-current="page" href="/ejemplo/"><i class="bi bi-house-fill me-2"></i>Inicio</a>
+                        <a class="nav-link" aria-current="page" href="/is3_martinez_lucas_"><i class="bi bi-house-fill me-2"></i>Inicio</a>
                     </li>
   
                     <div class="nav-item dropdown " >
                         <a class="nav-link dropdown-toggle" href="#" data-bs-toggle="dropdown">
-                            <i class="bi bi-gear me-2"></i>Dropdown
+                            <i class="bi bi-gear me-2"></i>Opciones
                         </a>
                         <ul class="dropdown-menu  dropdown-menu-dark "id="dropwdownRevision" style="margin: 0;">
                             <!-- <h6 class="dropdown-header">Información</h6> -->
                             <li>
-                                <a class="dropdown-item nav-link text-white " href="/is3_martinez_lucas_/dependencias/estadisticas"><i class="ms-lg-0 ms-2 bi bi-plus-circle me-2"></i>Dependencias</a>
+                                <a class="dropdown-item nav-link text-white " href="/is3_martinez_lucas_/dependencias/estadisticas"><i class="ms-lg-0 ms-2 bi bi-plus-circle me-2"></i>Estadística Dependencias</a>
                             </li>
                     
                         
@@ -44,7 +44,7 @@
                 </ul> 
                 <div class="col-lg-1 d-grid mb-lg-0 mb-2">
                     <!-- Ruta relativa desde el archivo donde se incluye menu.php -->
-                    <a href="/menu/" class="btn btn-danger"><i class="bi bi-arrow-bar-left"></i>MENÚ</a>
+                    <a href="/is3_martinez_lucas_" class="btn btn-danger"><i class="bi bi-arrow-bar-left"></i>MENÚ</a>
                 </div>
 
             
